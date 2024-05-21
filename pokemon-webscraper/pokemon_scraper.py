6ecafefd2ee9c04bb9ca775de7e3aa50c37ff1c9
alexiuwu <Alexander.Boosmann@gmail.com>
2024-05-21 22:46:23 +0200

    removing non-usable characters
    some pokémon (e.g. Nidoran) have characters like gender symbols or whatever. They're now gone. Another problem is, one runs the pokemon webscraper, the last line of the generated txt file ends with like "um". I have no idea why it does this, but manually removing it does its job. Now the list is showing Nidoran twice, but it doesn't matter since at the end, the collection of the valid pokémon(s) is a set.

--- pokemon-webscraper/pokemon_scraper.py
+++ pokemon-webscraper/pokemon_scraper.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import string as s
+
+allowed_letters = set(s.ascii_letters).union(set("äöü-ÄÖÜ0123456789ßé:"))
+
 
 if __name__ == "__main__":
     url = "https://www.pokewiki.de/Pok%C3%A9mon-Liste"
@@ -12,9 +16,14 @@ if __name__ == "__main__":
     with open("resources/german-pokemon.txt", "w", encoding="utf-8") as f:
         for i, line in enumerate(text):
             if line.strip().isnumeric():
-                f.write(f"{text[i+1]}\n")
+                name = []
+                for letter in text[i+1]:
+                    if letter in allowed_letters: # filtering for certain characters
+                        name += letter
+                
+                f.write(f"{"".join(name)}\n")
 
 
-    # just run it, if there are new pokemon released 
-    # Only intended for german names, to compute english names, change the last line to: f.write(f"{text[i+2]}\n)
-    # no guarantee that it works perfectly
+# just run it, if there are new pokemon released 
+# Only intended for german names, to compute english names, change the last line to: text[i+2]
+# no guarantee that it works perfectly
